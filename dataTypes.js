4139a97dc6afd022cd8f185a83757817620e5dde
4waj <4waj@fresnou.org>
2023-02-06 13:37:42 -0800

    initial commit

---
+++ dataTypes.js
@@ -0,0 +1,50 @@
+console.log(7);
+
+//this is a comment
+
+//Intergers
+console.log(-3);
+console.log(6);
+console.log(7);
+
+//Floating point numbers (floats)
+console.log(3.14);
+console.log(0.123456);
+
+//strings
+//characters, letters, symbols, spaces
+
+console.log("hello world!");
+console.log('!@#$ is also a string');
+console.log(`backticks by the 1`);
+
+//Arithmetic Operators (math)
+console.log(12+2);
+console.log(12-2);
+console.log(5*2); //shift + 8 = multiply
+console.log(4/2);
+console.log(14%2); //its called MOD
+                  // % means remained after dividing
+
+//Concatenation means to combine strings
+console.log("my name is"+"John Doe");
+console.log('my '+' '+'age'+' '+'is' + 14+'.');
+console.log("Your age is \n16.")
+console.log("your age is /n 14")
+
+//Template literal = plug in some calculated value
+//Use backticks `` by the 1 on your keyboard
+console.log(`15 divided by 10 = ${15/10} asdfasdf`);
+
+// Urnary Operators use one value
+console.log(typeof 15);
+console.log(typeof true);
+
+console.log(10 > 9); //greater than
+console.log(10 < 11); //less than
+console.log(10 >= 5); //greater than or equal too
+console.log(5 <= 8); // less than or equal too
+console.log(10==10); //are they both equal? true
+console.log(10!=5); //are they differnt? True
+console.log(16!=16); //are they different false
+//logical opperators
